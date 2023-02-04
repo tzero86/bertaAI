618c320a1682b261fd777b87ce94357727b0ed34
tzero86 <eliasmedina86@gmail.com>
2023-02-04 11:32:48 -0300

    BE URL Added

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch the data from the server, aka the AI response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://bertaai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
